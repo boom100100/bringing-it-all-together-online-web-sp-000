ee343482586a2a4c1d97137506e4bd4e0a8244fc
Bernadette Davis <bsd231@nyu.edu>
2019-09-03 18:25:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,64 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(hash)
+    @name = hash[:name]
+    @breed = hash[:breed]
+    @id = hash[:id]
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS dogs;
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+
+    sql = <<-SQL
+    INSERT INTO dogs(name, breed) VALUES(?,?);
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+
+    @id = DB[:conn].execute("SELECT * FROM dogs GROUP BY id HAVING MAX(id)").flatten[0]
+    self
+  end
+
+  def self.create(hash)
+    newDog = Dog.new(hash)
+    newDog.save
+    newDog
+
+  end
+
+  def self.new_from_db(row)
+    #puts row
+    hash = {:id => row[0], :name => row[1], :breed => row[2]}
+    newDog = Dog.new(hash)
+
+  end
+
+  def self.find_by_id
+  end
+  def self.find_or_create_by
+  end
+  def self.find_by_name
+  end
+  def update
+  end
+
+
+
+end
