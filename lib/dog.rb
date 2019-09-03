a4a64b59f5883f01f7f08f8164d0a61d42804983
Bernadette Davis <bsd231@nyu.edu>
2019-09-03 18:59:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,12 +60,12 @@ class Dog
     row = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?;", hash[:name], hash[:breed])[0]
 
     if !row.empty?
-
       dog_hash = {:id => row[0], :name => row[1], :breed => row[2]}
       dog = Dog.new(dog_hash)
     else
       dog = self.create(name: name, breed: breed)
     end
+    
     dog
 
   end
