d1a65d55df08d45d033a0ecabbe00e83846cc29f
Bernadette Davis <bsd231@nyu.edu>
2019-09-03 18:58:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
 
     if !row.empty?
 
-      dog_hash = [:id => row[0], :name => row[1], :breed => row[2]]
+      dog_hash = {:id => row[0], :name => row[1], :breed => row[2]}
       dog = Dog.new(dog_hash)
     else
       dog = self.create(name: name, breed: breed)
