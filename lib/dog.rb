1d575c7ce30bfed80f5e2e9867454f198a3369a9
Bernadette Davis <bsd231@nyu.edu>
2019-09-03 18:37:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,19 +58,14 @@ class Dog
   end
   def self.find_or_create_by(hash)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?;", hash[:name], hash[:breed])
-    #hash[:id] = result[0][0]
-    if result.nil?
-        return Dog.new(hash)
-      #else
-    end
-
+    
     if !song.empty?
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end
-    song
+    dog
 
 
 
